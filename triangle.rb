8a52a014997a8e040108ee16303152d165bdfdf3
Paul Samoylov <39446058+Paullsam@users.noreply.github.com>
2018-05-25 18:07:37 +0300

    Add files via upload

--- triangle.rb
+++ triangle.rb
@@ -1,15 +1,33 @@
-class Rectangle
+class Figure
+
+  def area
+    @side1 * @side2
+  end	
+
+  def perimeter
+    (@side1 + @side2) * 2
+  end
+  
+end
+  
+class Rectangle < Figure
     
-  def rectangle_area(side1: nil, side2: nil)
-    side1 * side2
+  def initialize(side1, side2)
+    @side1, @side2 = side1, side2
+  end
+   
+end
+
+class Square < Figure
+
+  def initialize(side)
+    @side1, @side2 = side, side 
   end
   
-  def square_area(side: nil)
-    side**2
-  end
+end
+
+class Triangle < Figure
   
-  # методы для треугольника
-      
   def oneside_triangle_area(side1: nil)
     Math.sqrt(3) / 4 * (side1**2)
   end
@@ -27,26 +45,34 @@ class Rectangle
     @angle3 = 180 - (angle1 + angle2)
 	(side1**2 / 2.0) * (Math.sin(angle1) * Math.sin(angle2) / Math.sin(@angle3))
   end
+
   
+=begin
+  def initialize(side1, side2, side3, angle1, angle2)
+    @side1, @side2, @side3, @angle1, @angle2 = side1, side2, side3, angle1, angle2
+  end
+
+ if side1 > 0 and side2 > 0 and side3 > 0
+    area = Triangle.tree_side_triangle_area
+ elsif side1 > 0 and side2 > 0 and angle1 > 0
+    area = Triangle.two_side_angle_triangle_area
+ elsif side1 > 0 and angle1 > 0 and angle2 > 0
+    area = Triangle.two_angle_side_triangle_area
+ else 
+    raise "Введите корректные данные"
+ end
+=end	
+	
 end
 
-class Square < Rectangle
-  
-  
-end
+r = Rectangle.new(3, 4)
+puts r.area
+puts r.perimeter
 
-class Triangle < Rectangle
-  
-  
-end
-
-
-r = Rectangle.new
-puts r.rectangle_area(side1: 3, side2: 4)
-
-s = Square.new
-puts s.square_area(side: 5)
+s = Square.new(5)
+puts s.area
+puts s.perimeter
 
 t = Triangle.new
-puts t.two_side_angle_triangle_area(side1: 6, side2: 3, angle1: 30)
+puts t.tree_side_triangle_area(side1: 4, side2: 4, side3: 5)
 
