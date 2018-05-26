7bdddb157711664d723dece09d10810dfeab2be8
Paul Samoylov <39446058+Paullsam@users.noreply.github.com>
2018-05-26 03:25:55 +0300

    Add files via upload

--- triangle.rb
+++ triangle.rb
@@ -1,78 +1,80 @@
 class Figure
 
-  def area
-    @side1 * @side2
-  end	
+  def <=>(other)
+    unless other.is_a? Figure 
+      raise "`#{other.inspect}` should be type of `Figure`"
+    end
+
+    self.area <=> other.area
+  end
+
+  def <(other)
+    (self <=> other) == -1
+  end
+
+  def >(other)
+    (self <=> other) == 1
+  end
+
+  def ==(other)
+    (self <=> other) == 0
+  end
+
+  def !=(other)
+    (self <=> other) != 0
+  end
 
   def perimeter
-    (@side1 + @side2) * 2
+    sides.inject :+
   end
-  
 end
-  
+
 class Rectangle < Figure
-    
   def initialize(side1, side2)
     @side1, @side2 = side1, side2
   end
-   
+
+  def area
+    @side1 * @side2
+  end
+
+  def sides
+    [@side1, @side1, @side2, @side2]
+  end
 end
 
-class Square < Figure
-
+class Square < Rectangle
   def initialize(side)
-    @side1, @side2 = side, side 
+    @side1, @side2 = side, side
   end
-  
 end
 
 class Triangle < Figure
-  
-  def oneside_triangle_area(side1: nil)
-    Math.sqrt(3) / 4 * (side1**2)
-  end
-  
-  def tree_side_triangle_area(side1: nil, side2: nil, side3: nil)
-    @p = (side1 + side2 + side3) / 2.0 
-   Math.sqrt(@p * (@p - side1) * (@p - side2) * (@p - side3))
-  end
-  
-  def two_side_angle_triangle_area(side1: nil, side2: nil, angle1: nil)
-    (side1 * side2 * Math.sin(angle1)) / 2.0
-  end
-  
-  def two_angle_side_triangle_area(side1: nil, angle1: nil, angle2: nil)
-    @angle3 = 180 - (angle1 + angle2)
-	(side1**2 / 2.0) * (Math.sin(angle1) * Math.sin(angle2) / Math.sin(@angle3))
-  end
 
-  
-=begin
   def initialize(side1, side2, side3, angle1, angle2)
     @side1, @side2, @side3, @angle1, @angle2 = side1, side2, side3, angle1, angle2
+   
+   if @side1 > 0 and @side2 > 0 and @side3 > 0
+    @p = (side1 + side2 + side3) / 2.0
+   elsif @side1 > 0 and @angle1 > 0 and @angle2 > 0
+    @side2 = (@side1 * Math.sin(@angle1)) / Math.sin(@angle2)
+	@side3 = Math.sqrt(@side1**2 + @side2**2 - (2 * @side1 * @side2) * Math.cos(@angle1))
+    @p = (side1 + side2 + side3) / 2.0
+   elsif @side1 > 0 and @side2 > 0 and @angle1 > 0
+    @side3 = Math.sqrt(@side1**2 + @side2**2 - (2 * @side1 * @side2) * Math.cos(@angle1))
+    @p = (side1 + side2 + side3) / 2.0
+   else 
+    raise "Введите корректные данные"
+   end
   end
 
- if side1 > 0 and side2 > 0 and side3 > 0
-    area = Triangle.tree_side_triangle_area
- elsif side1 > 0 and side2 > 0 and angle1 > 0
-    area = Triangle.two_side_angle_triangle_area
- elsif side1 > 0 and angle1 > 0 and angle2 > 0
-    area = Triangle.two_angle_side_triangle_area
- else 
-    raise "Введите корректные данные"
- end
-=end	
-	
+  def area
+    Math.sqrt(@p * (@p - @side1) * (@p - @side2) * (@p - @side3))
+  end
+
+  def sides
+    [@side1, @side2, @side3]
+  end
+
 end
-
-r = Rectangle.new(3, 4)
-puts r.area
-puts r.perimeter
-
-s = Square.new(5)
-puts s.area
-puts s.perimeter
-
-t = Triangle.new
-puts t.tree_side_triangle_area(side1: 4, side2: 4, side3: 5)
 
--- triangle_area2.rb
+++ triangle_area2.rb
@@ -1,38 +1,31 @@
 class Triangle
-
- # def initialize(side1, side2, side3, angle1, angle2)
- #   @side1, @side2, @side3, @angle1, @angle2 = side1, side2, side3, angle1, angle2
- # end  
- 
-  def oneside_triangle_area(side1: 0)
-    Math.sqrt(3) / 4 * (side1**2)
-  end
   
-  def tree_side_triangle_area(side1: 0, side2: 0, side3: 0)
-    @p = (side1 + side2 + side3) / 2.0 
-   Math.sqrt(@p * (@p - side1) * (@p - side2) * (@p - side3))
-  end
-  
-  def two_side_angle_triangle_area(side1: 0, side2: 0, angle1: 0)
-    (side1 * side2 * Math.sin(angle1)) / 2.0
-  end
-  
-  def two_angle_side_triangle_area(side1: 0, angle1: 0, angle2: 0)
-    @angle3 = 180 - (angle1 + angle2)
-	(side1**2 / 2.0) * (Math.sin(angle1) * Math.sin(angle2) / Math.sin(@angle3))
-  end
-
- if side1 > 0 and side2 > 0 and side3 > 0
-    area = Triangle.tree_side_triangle_area
- elsif side1 > 0 and side2 > 0 and angle1 > 0
-    area = Triangle.two_side_angle_triangle_area
- elsif side1 > 0 and angle1 > 0 and angle2 > 0
-    area = Triangle.two_angle_side_triangle_area
- else 
+  def initialize(side1, side2, side3, angle1, angle2)
+    @side1, @side2, @side3, @angle1, @angle2 = side1, side2, side3, angle1, angle2
+   
+   if @side1 > 0 and @side2 > 0 and @side3 > 0
+    @p = (side1 + side2 + side3) / 2.0
+   elsif @side1 > 0 and @angle1 > 0 and @angle2 > 0
+    @side2 = (@side1 * Math.sin(@angle1)) / Math.sin(@angle2)
+	@side3 = Math.sqrt(@side1**2 + @side2**2 - (2 * @side1 * @side2) * Math.cos(@angle1))
+    @p = (side1 + side2 + side3) / 2.0
+   elsif @side1 > 0 and @side2 > 0 and @angle1 > 0
+    @side3 = Math.sqrt(@side1**2 + @side2**2 - (2 * @side1 * @side2) * Math.cos(@angle1))
+    @p = (side1 + side2 + side3) / 2.0
+   else 
     raise "Введите корректные данные"
- end
-	
+   end
+  end
+
+  def area
+    Math.sqrt(@p * (@p - @side1) * (@p - @side2) * (@p - @side3))
+  end
+
+  def sides
+    [@side1, @side2, @side3]
+  end
 end
 
-t = Triangle.new
-puts t.area(side1: 3, side2: 3, side3: 5)
+t = Triangle.new(4, 8, 0, 35, 70)
+puts t.area
+puts t.sides
