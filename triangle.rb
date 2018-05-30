f95eb5a4850e44817ffc5a15bf7ad823b9ffc247
Paullsam <paulsammm@gmail.com>
2018-05-30 22:46:33 +0300

    changes

--- triangle.rb
+++ triangle.rb
@@ -48,18 +48,17 @@ class Triangle < Figure
   def initialize(side1:, side2: nil, side3: nil, angle1: nil, angle2: nil)
     @side1, @side2, @side3, @angle1, @angle2 = side1, side2, side3, angle1, angle2
 
-  def angles
     if @side1 && @side2 && @side3
-      3.times.map { |i| sides.rotate(i) }.map { |(s1, s2, s3)| Math.acos((s1**2 + s2**2 - s3**2) / (2.0 * s1 * s2)) }.map { |a| a * (180.0 / Math::PI) }
-    end
-  end
+      angles
 
-    if @side1 && @angle1 && @angle2
+    elsif @side1 && @angle1 && @angle2
       @side2 = (@side1 * Math.sin(@angle1)) / Math.sin(@angle2)
       @side3 = find_side3
+      @angle3 = find_angle3
 
     elsif @side1 && @side2 && @angle1
       @side3 = find_side3
+      angles
 
     else
       raise Figure::Error, "Введите корректные данные"
@@ -78,14 +77,22 @@ class Triangle < Figure
     [@side1, @side2, @side3]
   end
 
+  def angles
+    3.times.map { |i| sides.rotate(i) }.map { |(s1, s2, s3)| (Math.acos((s1**2 + s2**2 - s3**2) / (2.0 * s1 * s2))) * (180.0 / Math::PI) }
+  end
+
   private
 
   def p
-    @p = perimeter / 2.0
+    @p ||= perimeter / 2.0
   end
 
   def find_side3
     Math.sqrt(@side1**2 + @side2**2 - (2 * @side1 * @side2) * Math.cos(@angle1))
+  end
+
+  def find_angle3
+    @angle3 = 180.0 - (@angle1 + @angle2)
   end
 end
 
@@ -149,9 +156,6 @@ RSpec.describe Triangle do
   context 'when wrong Triangle' do
     let(:args) { {side1: 3, side2: 3, side3: 6} }
 
-    it do
-      expect { described_class.new(**args) }.
-        to raise_error(Figure::Error, 'Неправильный треугольник')
-    end
+    it { expect{ described_class.new(**args) }.to raise_error(Figure::Error, 'Неправильный треугольник') }
   end
 end
