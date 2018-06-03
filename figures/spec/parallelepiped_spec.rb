a53b007a286dc98dcf06c43bd16d3b1c0e2ccc15
Paullsam <paulsammm@gmail.com>
2018-06-03 15:56:55 +0300

    Исправил параллелепипед добавил строку в .rcpec добавил тесты в пирамиды

--- figures/spec/parallelepiped_spec.rb
+++ figures/spec/parallelepiped_spec.rb
@@ -2,13 +2,13 @@ RSec.describe Parallelepiped do
   subject { describe_class.new(**args) }
 
   context 'Если куб'
-    let(:args) {side1: 4, side2: 4, height: 4}
+    let(:args) { {side1: 4, side2: 4, height: 4} }
     it { expect{subject.area}.to eq 96 }
     it { expect{subject.volume}.to eq 64 }
   end
 
   context 'Если прямой параллелепипед'
-    let(:args) {side1: 3, side2: 3, height: 6}
+    let(:args) { {side1: 3, side2: 3, height: 6} }
     it { expect{subject.area}.to eq 90 }
     it { expect{subject.volume}.to eq 54 }
   end
--- figures/spec/pyramid_spec.rb
+++ figures/spec/pyramid_spec.rb
@@ -7,4 +7,16 @@ Rcpec.describe Pyramid do
     it { expect(subject.area).to eq 98 }
     it { expect(subject.volume).to eq 53.3333 }
   end
+
+  contex 'Пирамида с треугольным основанием'
+    let(:args) { {height: 4, base: triangle, side: 3} }
+
+    it { expect(subject.apothem).to eq 5 }
+  end
+
+  contex "Error"
+    let(:args) { {height: 15, base: квадрат, side: 5} }
+
+    it { expect{ describe_class.new(**args)}.to raise_error(Figure::Error, 'Введите тип основания') }
+  end
 end
